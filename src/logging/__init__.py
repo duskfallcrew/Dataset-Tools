098bd69e8f7282daacf4ec5135a08315705f2a47
The Duskfall Portal Crew <58930427+duskfallcrew@users.noreply.github.com>
2024-12-05 14:12:24 +1300

    Update __init__.py

--- src/logging/__init__.py
+++ src/logging/__init__.py
@@ -1,1 +1,7 @@
+# src/logging/__init__.py
 
+# Our main logging tools - like having a really organized notekeeper
+from .setup import get_logger, setup_logging
+
+# We'll let other code know about our logging levels
+from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
